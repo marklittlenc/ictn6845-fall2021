32c4b0a64439625e10991994ceb319e950178e2b
marklittlenc <81596257+marklittlenc@users.noreply.github.com>
2021-12-02 18:44:52 -0500

    Add files via upload

---
+++ home_view.php
@@ -0,0 +1,16 @@
+<?php include 'view/header.php'; ?>
+<?php include 'view/sidebar.php'; ?>
+<main class="nofloat">
+    <h1>Welcome</h1>
+<img src="images/1.jpg" width=70% height=70% border=1 vspace=3 alt="'We Are One'; a Pitt County Cultural Center commission by local artist Jean Bertrand." align=left>
+<p>
+<i>"We Are One"; a Pitt County Cultural Center commission by local artist Jean Bertrand.</i>     
+<p>The Pitt County Cultural Center is a multicultural museum and social event center located in Greenville, NC, and founded in 1987 by Ruth Ann Butler. The Center provides a haven of historical and multicultural reflection, communication, collaboration, research, and education.
+</p>
+<p>Exhibits, archives, guided tours, and meeting spaces are continually improved and curated to offer even greater resources for those seeking to be inspired and enriched by gaining a knowledge and understanding of the Greenville area’s multicultural diversity and its history of connecting community and culture.
+</p>
+<p>Our facility boasts a wide selection of rooms that may be rented by the day. To make a reservation, first create an account by selecting the “Login/Register” link at left, then browse the rooms and submit your reservation.
+</p>
+
+</main>
+<?php include 'view/footer.php'; ?>
